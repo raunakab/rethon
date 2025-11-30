3b904529d1b47885ba9ba6b8f0374b39c0d03466
Raunak Bhagat <r@rabh.io>
2025-11-29 23:06:20 -0700

    Implement higher-level token definition

--- libs/aggregator/src/lib.rs
+++ libs/aggregator/src/lib.rs
@@ -1,1 +1,67 @@
+pub fn aggregator<'a>(tokenizer: impl Iterator<Item = tokenizer::Token<'a>>) {}
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Token<'a> {
+    MacroIdentifier(&'a str),
+    Identifier(&'a str),
+    Number(&'a str),
+
+    // Keywords
+    Function, // fn
+    Scope,    // scope
+    Return,   // return
+    Yield,    // yield
+    Not,      // not
+    And,      // and
+    Or,       // or
+    For,      // for
+    Loop,     // loop
+    If,       // if
+    Else,     // else
+    True,     // true
+    False,    // false
+
+    // Operators
+    Assignment,         // =
+    Equals,             // ==
+    Promotion,          // !
+    Coalescence,        // ?
+    Ampersand,          // @
+    Colon,              // :
+    Dot,                // .
+    DoubleDot,          // ..
+    Plus,               // +
+    Minus,              // -
+    DoubleMinus,        // --
+    Arrow,              // ->
+    Asterisk,           // *
+    DoubleAsterisk,     // **
+    Slash,              // /
+    Pipe,               // |
+    PipeForward,        // |>
+    PipeDoubleForward,  // |>>
+    PipeDoubleBackward, // |<<
+
+    Greater,        // >
+    DoubleGreater,  // >>
+    GreaterOrEqual, // >=
+    Lesser,         // <
+    DoubleLesser,   // <<
+    LesserOrEqual,  // <=
+
+    // Keywords
+    Brace(Brace, BraceDirection),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Brace {
+    Round,
+    Square,
+    Curly,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum BraceDirection {
+    Open,
+    Close,
+}
