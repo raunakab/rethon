c3bd89e174f84110cca89ea7aa6ec222f19f1867
Raunak Bhagat <r@rabh.io>
2025-11-30 01:04:24 -0700

    Finish aggregator function

--- libs/aggregator/src/lib.rs
+++ libs/aggregator/src/lib.rs
@@ -1,22 +1,22 @@
-pub fn aggregator<'a>(
-    tokenizer: impl Iterator<Item = tokenizer::Token<'a>>,
-) -> impl Iterator<Item = Token<'a>> {
-    Aggregator::from(tokenizer)
+pub fn aggregator<'a>(source: &'a str) -> impl Iterator<Item = Token<'a>> {
+    Aggregator::new(source, tokenizer::tokenize(source))
 }
 
 struct Aggregator<'a, I>
 where
     I: Iterator<Item = tokenizer::Token<'a>>,
 {
+    source: &'a str,
     tokenizer: std::iter::Peekable<I>,
 }
 
-impl<'a, I> From<I> for Aggregator<'a, I>
+impl<'a, I> Aggregator<'a, I>
 where
     I: Iterator<Item = tokenizer::Token<'a>>,
 {
-    fn from(tokenizer: I) -> Self {
+    fn new(source: &'a str, tokenizer: I) -> Self {
         Self {
+            source,
             tokenizer: tokenizer.peekable(),
         }
     }
@@ -31,148 +31,122 @@ where
     fn next(&mut self) -> Option<Self::Item> {
         // Helper macro to peek and match on next token
         // Consumes the token if it matches any of the patterns
+        // Automatically wraps tuple patterns in Some()
         macro_rules! peek {
-            ($($pattern:pat => $result:expr),+ $(,)?) => {{
-                let next_token = self.tokenizer.peek().map(|t| (t.token, t.token_type));
+            ($(($($inner:tt)*) => $result:expr),+ $(, _ => $default:expr)? $(,)?) => {{
+                let next_token = self.tokenizer.peek().map(|t| (t.token, t.range.clone(), t.token_type));
                 match next_token {
                     $(
-                        $pattern => {
+                        Some(($($inner)*)) => {
                             self.tokenizer.next();
                             $result
                         }
                     )+
+                    $(
+                        _ => $default
+                    )?
                 }
             }};
         }
 
         loop {
-            match self.tokenizer.next() {
-                Some(tokenizer::Token { token, token_type }) => {
-                    break Some(match token_type {
-                        tokenizer::TokenType::Whitespace => match token {
-                            "\n" => Token::Newline,
-                            "\t" => Token::Tab,
-                            _ => continue, // Skip other whitespace (spaces, etc.)
-                        },
-                        tokenizer::TokenType::Keyword => match token {
-                            "fn" => Token::Function,
-                            "scope" => Token::Scope,
-                            "return" => Token::Return,
-                            "yield" => Token::Yield,
-                            "not" => Token::Not,
-                            "and" => Token::And,
-                            "or" => Token::Or,
-                            "for" => Token::For,
-                            "loop" => Token::Loop,
-                            "if" => Token::If,
-                            "else" => Token::Else,
-                            "true" => Token::True,
-                            "false" => Token::False,
-                            _ => Token::Identifier(token),
-                        },
-                        tokenizer::TokenType::Numeric => {
-                            // Check if this is a floating point number (e.g., 12. or 12.34)
-                            if let Some(next) = self.tokenizer.peek() {
-                                if next.token_type == tokenizer::TokenType::Punctuation
-                                    && next.token == "."
-                                {
-                                    let start_ptr = token.as_ptr();
+            let Some(tokenizer::Token {
+                token,
+                range,
+                token_type,
+            }) = self.tokenizer.next()
+            else {
+                break None;
+            };
 
-                                    // Consume the dot
-                                    let dot = self.tokenizer.next().unwrap();
-
-                                    // Check if there's a number after the dot (e.g., 12.34)
-                                    let end_ptr = if let Some(after_dot) = self.tokenizer.peek() {
-                                        if after_dot.token_type == tokenizer::TokenType::Numeric {
-                                            // Consume the fractional part
-                                            let frac = self.tokenizer.next().unwrap();
-                                            // End pointer is at the end of the fractional part
-                                            unsafe { frac.token.as_ptr().add(frac.token.len()) }
-                                        } else {
-                                            // Just number with trailing dot (e.g., 12.)
-                                            unsafe { dot.token.as_ptr().add(dot.token.len()) }
-                                        }
-                                    } else {
-                                        // Just number with trailing dot (e.g., 12.)
-                                        unsafe { dot.token.as_ptr().add(dot.token.len()) }
-                                    };
-
-                                    // Calculate the full slice from start to end
-                                    let len = unsafe { end_ptr.offset_from(start_ptr) as usize };
-                                    let float_str = unsafe {
-                                        std::str::from_utf8_unchecked(std::slice::from_raw_parts(
-                                            start_ptr, len,
-                                        ))
-                                    };
-
-                                    Token::Number(float_str)
-                                } else {
-                                    Token::Number(token)
-                                }
-                            } else {
-                                Token::Number(token)
-                            }
+            break Some(match token_type {
+                tokenizer::TokenType::Whitespace => match token {
+                    "\n" => Token::Newline,
+                    "\t" => Token::Tab,
+                    _ => continue, // Skip other whitespace (spaces, etc.)
+                },
+                tokenizer::TokenType::Keyword => match token {
+                    "fn" => Token::Function,
+                    "scope" => Token::Scope,
+                    "return" => Token::Return,
+                    "yield" => Token::Yield,
+                    "not" => Token::Not,
+                    "and" => Token::And,
+                    "or" => Token::Or,
+                    "for" => Token::For,
+                    "loop" => Token::Loop,
+                    "if" => Token::If,
+                    "else" => Token::Else,
+                    "true" => Token::True,
+                    "false" => Token::False,
+                    _ => Token::Identifier(token),
+                },
+                tokenizer::TokenType::Numeric => peek! {
+                    (".", dot_range, _) => {
+                        peek! {
+                            (_, frac_range, tokenizer::TokenType::Numeric) => Token::Number(&self.source[range.start..frac_range.end]),
+                            _ => Token::Number(&self.source[range.start..dot_range.end]),
                         }
-                        tokenizer::TokenType::Punctuation => match token {
-                            "=" => peek! {
-                                Some(("=", _)) => Token::Equals,
-                                _ => Token::Assignment,
-                            },
-                            "!" => peek! {
-                                Some((ident, tokenizer::TokenType::Keyword)) => Token::MacroIdentifier(ident),
-                                _ => Token::Promotion,
-                            },
-                            "?" => Token::Coalescence,
-                            "@" => Token::Ampersand,
-                            ":" => peek! {
-                                Some(("=", _)) => Token::ConstantAssignment,
-                                _ => Token::Colon,
-                            },
-                            "." => peek! {
-                                Some((".", _)) => Token::DoubleDot,
-                                _ => Token::Dot,
-                            },
-                            "+" => Token::Plus,
-                            "-" => peek! {
-                                Some(("-", _)) => Token::DoubleMinus,
-                                Some((">", _)) => Token::Arrow,
-                                _ => Token::Minus,
-                            },
-                            "*" => peek! {
-                                Some(("*", _)) => Token::DoubleAsterisk,
-                                _ => Token::Asterisk,
-                            },
-                            "/" => Token::Slash,
-                            "|" => peek! {
-                                Some((">", _)) => peek! {
-                                    Some((">", _)) => Token::PipeDoubleForward,
-                                    _ => Token::PipeForward,
-                                },
-                                _ => Token::Pipe,
-                            },
-                            ">" => peek! {
-                                Some(("=", _)) => Token::GreaterOrEqual,
-                                Some((">", _)) => Token::DoubleGreater,
-                                _ => Token::Greater,
-                            },
-                            "<" => peek! {
-                                Some(("=", _)) => Token::LesserOrEqual,
-                                Some(("<", _)) => Token::DoubleLesser,
-                                _ => Token::Lesser,
-                            },
-                            "(" => Token::Brace(Brace::Round, BraceDirection::Open),
-                            ")" => Token::Brace(Brace::Round, BraceDirection::Close),
-                            "[" => Token::Brace(Brace::Square, BraceDirection::Open),
-                            "]" => Token::Brace(Brace::Square, BraceDirection::Close),
-                            "{" => Token::Brace(Brace::Curly, BraceDirection::Open),
-                            "}" => Token::Brace(Brace::Curly, BraceDirection::Close),
-                            _ => continue, // Skip unknown punctuation
+                    },
+                    _ => Token::Number(token),
+                },
+                tokenizer::TokenType::Punctuation => match token {
+                    "=" => peek! {
+                        ("=", ..) => Token::Equals,
+                        _ => Token::Assignment,
+                    },
+                    "!" => peek! {
+                        (ident, _, tokenizer::TokenType::Keyword) => Token::MacroIdentifier(ident),
+                        _ => Token::Promotion,
+                    },
+                    "?" => Token::Coalescence,
+                    "@" => Token::Ampersand,
+                    ":" => peek! {
+                        ("=", ..) => Token::ConstantAssignment,
+                        _ => Token::Colon,
+                    },
+                    "." => peek! {
+                        (".", ..) => Token::DoubleDot,
+                        _ => Token::Dot,
+                    },
+                    "+" => Token::Plus,
+                    "-" => peek! {
+                        ("-", ..) => Token::DoubleMinus,
+                        (">", ..) => Token::Arrow,
+                        _ => Token::Minus,
+                    },
+                    "*" => peek! {
+                        ("*", ..) => Token::DoubleAsterisk,
+                        _ => Token::Asterisk,
+                    },
+                    "/" => Token::Slash,
+                    "|" => peek! {
+                        (">", ..) => peek! {
+                            (">", ..) => Token::PipeDoubleForward,
+                            _ => Token::PipeForward,
                         },
-                        tokenizer::TokenType::Unknown => continue,
-                    });
-                }
-                None => break None,
-            }
+                        _ => Token::Pipe,
+                    },
+                    ">" => peek! {
+                        ("=", ..) => Token::GreaterOrEqual,
+                        (">", ..) => Token::DoubleGreater,
+                        _ => Token::Greater,
+                    },
+                    "<" => peek! {
+                        ("=", ..) => Token::LesserOrEqual,
+                        ("<", ..) => Token::DoubleLesser,
+                        _ => Token::Lesser,
+                    },
+                    "(" => Token::Brace(Brace::Round, BraceDirection::Open),
+                    ")" => Token::Brace(Brace::Round, BraceDirection::Close),
+                    "[" => Token::Brace(Brace::Square, BraceDirection::Open),
+                    "]" => Token::Brace(Brace::Square, BraceDirection::Close),
+                    "{" => Token::Brace(Brace::Curly, BraceDirection::Open),
+                    "}" => Token::Brace(Brace::Curly, BraceDirection::Close),
+                    _ => continue, // Skip unknown punctuation
+                },
+                tokenizer::TokenType::Unknown => continue,
+            });
         }
     }
 }
