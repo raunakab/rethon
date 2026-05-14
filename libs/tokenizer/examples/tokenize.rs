de562feacd650fe34dab5eff12bb198ca02a0816
Raunak Bhagat <r@rabh.io>
2026-05-14 10:08:12 -0700

    Spruce up example

--- libs/tokenizer/examples/tokenize.rs
+++ libs/tokenizer/examples/tokenize.rs
@@ -1,39 +1,42 @@
 use tokenizer::{Token, tokenize};
 
-static SOURCE: &str = "\
-fn greet
-    name := \"world\"
-    msg := f\"Hello\"
-    return msg";
+static SOURCE: &str = "
+num = rand()
+
+fn main
+    x = 1
+    y = 2
+    z =
+        if x + y > num
+            x + y
+        else
+            throw
+
+    result = z ** 2
+    result
+";
 
 fn main() {
     println!("=== Source ===");
     println!("{SOURCE}");
+    println!();
+
     println!("\n=== Token stream ===");
 
     let mut depth = 0usize;
     for result in tokenize(SOURCE) {
+        let result = result.unwrap();
         match result {
-            Ok(Token::ScopeStart(_)) => {
-                println!("{}ScopeStart", "    ".repeat(depth));
-                depth += 1;
+            Token::ScopeStart(_) => {
+                println!("{}{{", "    ".repeat(depth));
+                depth = depth.saturating_add(1);
             }
-            Ok(Token::ScopeEnd(_)) => {
+            Token::ScopeEnd(_) => {
                 depth = depth.saturating_sub(1);
-                println!("{}ScopeEnd", "    ".repeat(depth));
+                println!("{}}}", "    ".repeat(depth));
             }
-            Ok(Token::Token(ty, pos)) => {
-                let text = &SOURCE[pos.source_range];
-                println!(
-                    "{}{ty:<16}  line {:>2}  col {:>2}  {text:?}",
-                    "    ".repeat(depth),
-                    pos.line,
-                    pos.line_range.start,
-                );
-            }
-            Err(e) => {
-                eprintln!("Error: {e}");
-                std::process::exit(1);
+            Token::Token(ty, _) => {
+                println!("{}{ty}", "    ".repeat(depth));
             }
         }
     }
